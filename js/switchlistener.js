3fad25fe22c1c3d4b79887f52d1f6917a26eca3f
itsmarsss <itsmarzzzzzz@gmail.com>
2022-08-27 19:37:26 -0400

    Add session theme storage

--- js/switchlistener.js
+++ js/switchlistener.js
@@ -1,5 +1,17 @@
 const tswitch = document.getElementById("switch");
-
+try {
+    if (window.sessionStorage.getItem("dark") == "true") {
+        toggle();
+        tswitch.click();
+    }
+} catch (error) { }
+let dark = false;
 tswitch.addEventListener("mousedown", (event) => {
+    toggle();
+});
+
+function toggle() {
     document.body.classList.toggle('dark-theme');
-}); 
+    dark = !dark;
+    window.sessionStorage.setItem("dark", dark);
+}
