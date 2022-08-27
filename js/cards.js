8010de7884f62d5b3ee6d212fbe195ab2959b2a5
itsmarsss <itsmarzzzzzz@gmail.com>
2022-08-27 09:21:01 -0400

    Intial commit

---
+++ js/cards.js
@@ -0,0 +1,34 @@
+const cards = document.getElementById("cards");
+
+function selected(site) {
+    window.open(site, "_blank");
+}
+
+loadMore();
+
+function loadMore() {
+    for (let i = 0; i < 12; i++) {
+        let link = 'https://www.google.com/';
+        let name = 'Name';
+        let pfp = 'assets/placeholder.png';
+        let about = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi'
+
+        cards.innerHTML +=
+            `<div class="card show-on-scroll">
+                <div class="user" id="link" onclick="selected('${link}')">
+                    <div class="top">
+                        <img class="pfp" src="${pfp}" width="50" height="50"></td>
+
+                        <h3 class="name">
+                            ${name}
+                        </h3>
+                    </div>
+                    <div class="bottom">
+                        <h5 class="about">
+                            About: <em>${about}</em>
+                        </h5>
+                    </div>
+                </div>
+            </div>`
+    }
+}
