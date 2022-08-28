58960911856c356ec0f68733c50c152550fc8381
itsmarsss <itsmarzzzzzz@gmail.com>
2022-08-28 01:11:07 -0400

    Update template

--- js/profilecheck.js
+++ js/profilecheck.js
@@ -1,3 +1,11 @@
 if (getCookie("auth") != "true") {
     window.location = "/NetUs/login.html";
 }
+
+function updateLocal() {
+    const pfp = document.getElementById("pfp");
+    const aboutme = document.getElementById("aboutme");
+    pfp.innerHTML = `<img id="pfp" src="${getCookie("profilepicture")}" alt="pfp" width="100" height="100">`
+    
+    aboutme.innerHTML = `<p id="aboutme">${getCookie("aboutme")}</p>`
+}
