9316c7d986108a1f5ce6352bd66d634ff3b5174e
itsmarsss <itsmarzzzzzz@gmail.com>
2022-08-28 01:53:16 -0400

    Fix typo

--- js/profilecheck.js
+++ js/profilecheck.js
@@ -7,7 +7,7 @@ updateLocal();
 function updateLocal() {
     const pfp = document.getElementById("pfp");
     const aboutme = document.getElementById("aboutme");
-    pfp.innerHTML = `<img id="pfp" src="${getCookie("pfp")}" alt="pfp" width="100" height="100">`
+    pfp.outterHTML = `<img id="pfp" src="${getCookie("pfp")}" alt="pfp" width="100" height="100">`
     
-    aboutme.innerHTML = `<p id="aboutme">${getCookie("aboutme")}</p>`
+    aboutme.outterHTML = `<p id="aboutme">${getCookie("aboutme")}</p>`
 }
