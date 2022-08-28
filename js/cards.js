c861fc1ca4914a90b3d10ce85075379fa377b34e
itsmarsss <itsmarzzzzzz@gmail.com>
2022-08-28 12:12:15 -0400

    Show profiles

--- js/cards.js
+++ js/cards.js
@@ -4,17 +4,36 @@ function selected(site) {
     window.open(site, "_blank");
 }
 
+let users = [];
+
+getUsers();
+
+function getUsers() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", '/NetUs/UsersServlet', false);
+    xmlHttp.send(null);
+    const response = xmlHttp.responseText;
+    console.log(response);
+    users = JSON.parse(response)
+}
+
 loadMore();
 
 function loadMore() {
-    for (let i = 0; i < 12; i++) {
-        let link = 'https://itsmarsss.github.io/Calculator-app';
-        let name = 'Name';
-        let pfp = 'assets/placeholder.png';
-        let about = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi'
+    if (users.length == 0) {
+        alert('No more users!');
+    } else {
+        try {
+            for (var i = 0; i <= 12; i++) {
+                console.log(i);
+                let user = users.shift();
+                let link = hex_to_ascii(user.website);
+                let name = hex_to_ascii(user.username);
+                let pfp = hex_to_ascii(user.pfp); //'assets/placeholder.png'
+                let about = hex_to_ascii(user.aboutme); //'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi'
 
-        cards.innerHTML +=
-            `<div class="card show-on-scroll">
+                cards.innerHTML +=
+                    `<div class="card show-on-scroll">
                 <div class="user" id="link" onclick="selected('${link}')">
                     <div class="top">
                         <img class="pfp" src="${pfp}" width="50" height="50"></td>
@@ -30,5 +49,16 @@ function loadMore() {
                     </div>
                 </div>
             </div>`
+            }
+        } catch (error) { }
     }
+}
+
+function hex_to_ascii(str1) {
+    var hex = str1.toString();
+    var str = '';
+    for (var n = 0; n < hex.length; n += 2) {
+        str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+    }
+    return str;
 }
