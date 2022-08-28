70fef7c3db3e34ac9bd03b1de6ba199fc132b580
itsmarsss <itsmarzzzzzz@gmail.com>
2022-08-28 01:40:22 -0400

    Debug

--- js/switchlistener.js
+++ js/switchlistener.js
@@ -36,15 +36,21 @@ function setCookie(name, value, days) {
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie =name + "=" + (ascii_to_hex(value) || "") + expires + "; path=/";
+    document.cookie = name + "=" + (ascii_to_hex(value) || "") + expires + "; path=/";
 }
 function getCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return hex_to_ascii(c.substring(nameEQ.length, c.length));
+        while (c.charAt(0) == ' ')
+            c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) {
+            if (c.substring(nameEQ.length, c.length) == "true" || c.substring(nameEQ.length, c.length) == "false") {
+                return c.substring(nameEQ.length, c.length);
+            }
+            return hex_to_ascii(c.substring(nameEQ.length, c.length));
+        }
     }
     return null;
 }
