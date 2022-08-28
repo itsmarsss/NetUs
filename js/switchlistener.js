363cc5a527fe92b5d15f2db6787dc7b8d0a46ef4
itsmarsss <itsmarzzzzzz@gmail.com>
2022-08-28 01:27:55 -0400

    Added hex encoder and decoders

--- js/switchlistener.js
+++ js/switchlistener.js
@@ -36,7 +36,7 @@ function setCookie(name, value, days) {
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    document.cookie =name + "=" + (ascii_to_hex(value) || "") + expires + "; path=/";
 }
 function getCookie(name) {
     var nameEQ = name + "=";
@@ -44,7 +44,25 @@ function getCookie(name) {
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        if (c.indexOf(nameEQ) == 0) return hex_to_ascii(c.substring(nameEQ.length, c.length));
     }
     return null;
+}
+
+function hex_to_ascii(str1) {
+    var hex = str1.toString();
+    var str = '';
+    for (var n = 0; n < hex.length; n += 2) {
+        str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+    }
+    return str;
+}
+
+function ascii_to_hex(str) {
+    var arr1 = [];
+    for (var n = 0, l = str.length; n < l; n++) {
+        var hex = Number(str.charCodeAt(n)).toString(16);
+        arr1.push(hex);
+    }
+    return arr1.join('');
 }
